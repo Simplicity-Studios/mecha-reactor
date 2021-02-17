839ba444a75d41ebb49afccf2b08ed4e7b03a6e0
Sean Malloy <sfmalloy@millersville.edu>
2021-02-17 08:08:49 -0500

    Added individual reactor bar for each attribute

---
+++ MechaReactor/Assets/Scripts/UI/ReactorButton.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ReactorButton : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject player;
+    public Sprite[] fillBarSprites;
+
+    public string attributeName;
+    public float minValue, maxValue;
+
+    private int pointsAllocated;
+    private float step, maxLevel;
+    private ReactorAttributes stats;
+    private Image fillBar;
+
+    void Start()
+    {
+        maxLevel = fillBarSprites.Length - 1;
+        pointsAllocated = 0;
+        step = (maxValue - minValue) / maxLevel;
+
+        fillBar = gameObject.transform.GetChild(0).gameObject.GetComponent<Image>();
+        stats = player.GetComponent<ReactorAttributes>();
+        stats[attributeName] = Mathf.Clamp(stats[attributeName], minValue, maxValue);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left) 
+        {
+            stats[attributeName] += step;
+            pointsAllocated += 1;
+        }
+        if (eventData.button == PointerEventData.InputButton.Right) 
+        {
+            stats[attributeName] -= step;
+            pointsAllocated -= 1;
+        }
+        
+        stats[attributeName] = Mathf.Clamp(stats[attributeName], minValue, maxValue);
+        pointsAllocated = (int) Mathf.Clamp(pointsAllocated, 0, maxLevel);
+
+        fillBar.sprite = fillBarSprites[pointsAllocated];
+    }
+}
