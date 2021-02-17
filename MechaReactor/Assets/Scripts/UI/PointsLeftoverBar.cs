434a3d01c458d0ab7931f8901d15bc90d7a9b412
Sean Malloy <sfmalloy@millersville.edu>
2021-02-17 09:24:26 -0500

    Added main reactor bar that depletes when you allocate points to different stats

---
+++ MechaReactor/Assets/Scripts/UI/PointsLeftoverBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsLeftoverBar : MonoBehaviour
+{
+    public GameObject[] reactorButtons;
+    public Sprite[] sprites;
+
+    private int level;
+    private Image barSprite;
+
+    void Start()
+    {
+        level = reactorButtons.Length - 1;
+        barSprite = gameObject.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        int tempMax = sprites.Length - 1;
+        foreach (GameObject rButton in reactorButtons)
+            tempMax -= rButton.GetComponent<ReactorButton>().GetPointsAllocated();
+        level = tempMax;
+
+        barSprite.sprite = sprites[level];
+    }
+
+    public int GetLevel()
+    {
+        return level;
+    }
+}
