2484774adfb5e07621307848e57472210afac1af
Joshua Carney <48502124+jkcarney@users.noreply.github.com>
2021-02-22 13:47:04 -0500

    NavMesh2D library installed, awaiting implementation in main enemycontroller script

---
+++ MechaReactor/Assets/Scripts/Enemy/EnemySprinter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemySprinter : MonoBehaviour
+{
+    public Transform target;
+    // Give enemy units a "Nav Mesh Agent" component
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        // This code block needs to go in EVERY start function of a pathfinding agent.
+		agent = GetComponent<NavMeshAgent>();
+		agent.updateRotation = false;
+		agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Self explanatory, but set the agent's destination
+        agent.SetDestination(target.position); 
+    }
+}
