dda42e0f3f10e15340a363c4274337ceb328ade7
Amy Burnett <16aburnett@gmail.com>
2021-02-25 18:33:36 -0500

    Enemy Bullets now ignore other enemies

--- MechaReactor/Assets/Scripts/Enemy/EnemyBullet.cs
+++ MechaReactor/Assets/Scripts/Enemy/EnemyBullet.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class EnemyBullet : MonoBehaviour
 {
     public GameObject hitEffect;
+    public Collider2D collider; 
 
     private float dmg;
 
-    void OnCollisionEnter2D(Collision2D col)
+    void Start()
     {
+        collider = GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Collisions with the Player 
         if(col.transform.CompareTag("Player"))
         {
             col.gameObject.GetComponent<PlayerController>().TakeDamage(dmg);
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+            Destroy(gameObject);
         }
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 1f);
-        Destroy(gameObject);
+        
+        // Collide with walls and obstacles
+        else if (col.gameObject.layer == LayerMask.NameToLayer("Obstacles")) 
+        {
+            Debug.Log("Die wall!");
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+            Destroy(gameObject);
+        }
     }
 
     public void setBulletDamage(float newDmg)
