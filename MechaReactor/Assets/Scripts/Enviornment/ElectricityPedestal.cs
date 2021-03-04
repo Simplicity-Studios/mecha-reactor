6054e2af5adee67a549edbb9c8f1630a8ed226f1
Joshua Carney <48502124+jkcarney@users.noreply.github.com>
2021-03-03 22:58:12 -0500

    Not-scuffed UI

---
+++ MechaReactor/Assets/Scripts/Enviornment/ElectricityPedestal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricityPedestal : MonoBehaviour
+{
+    public float dropRate;
+    public GameObject electricityPrefab;
+    public Sprite NoElectricitySprite;
+    private Sprite ElectricitySprite;
+    private float timePickedUp;
+    private bool isEmpty;
+
+    void Start()
+    {   
+        ElectricitySprite = GetComponent<SpriteRenderer>().sprite;
+        SpawnElectricity();
+    }
+
+    void Update()
+    {
+        if(this.transform.childCount == 0 && !isEmpty)
+        {
+            timePickedUp = Time.time;
+            isEmpty = true;
+            GetComponent<SpriteRenderer>().sprite = NoElectricitySprite;
+        }
+        if(Time.time > timePickedUp + dropRate && isEmpty)
+        {
+            SpawnElectricity();
+        }
+    }
+
+    void SpawnElectricity()
+    {
+        GameObject powerup = Instantiate(electricityPrefab, new Vector3(transform.position.x, transform.position.y+0.5f, transform.position.z), Quaternion.identity);
+        powerup.transform.SetParent(this.transform);
+        isEmpty = false;
+        GetComponent<SpriteRenderer>().sprite = ElectricitySprite;
+    }
+}
