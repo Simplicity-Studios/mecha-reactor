c2dc794fe4ba344aeaed6fe65b8a49d381f559c9
Sean Malloy <sfmalloy@millersville.edu>
2021-03-03 22:16:24 -0500

    Scuffed UI

---
+++ MechaReactor/Assets/Scripts/UI/GameSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
